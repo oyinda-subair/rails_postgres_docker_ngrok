ae200c15c58d97c0ca5f3e3c71aaf88674f63590
oyinda-subair <oyinda.subair@gmail.com>
2023-07-10 22:20:37 +0100

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'health/index'
+  resources :secret_menu_items, only: %i[index show create update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
